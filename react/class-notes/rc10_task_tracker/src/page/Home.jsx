4ac48afe2d5f08fbc3c92c177d605802ecd5c57d
Mehmet Kabasakal <sehriistanbul@gmail.com>
2022-10-21 14:08:37 +0300

    end of the head

--- react/class-notes/rc10_task_tracker/src/page/Home.jsx
+++ react/class-notes/rc10_task_tracker/src/page/Home.jsx
@@ -1,14 +1,39 @@
 import React from "react";
 import AddTask from "../components/addTask/AddTask";
 import TaskList from "../components/taskList/TaskList";
-import Button from "..components/button/Button"
+import Button from "react-bootstrap/Button"
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [isOpen, setIsOpen]= useState(false)
+  const [text, setText]=useState("Show Task Bar")
+  const [task, setTask]= useState();
+  const url = "https://6351820a3e9fa1244e60840c.mockapi.io/api/tasks";
+
+  const toggle=(e)=>{
+    setIsOpen(!isOpen)
+    const buttonText=isOpen ? "Show Task Bar" : "Hide Task Bar"
+    setText(buttonText);
+  }
+
+  const getTask= async()=>{
+    const {data}=await axios(url)
+    setTask(data);
+    console.log(data);
+  }
+
+  useEffect(() => {
+    getTask()
+  },[])
   return (
     <div>
-      Home
-      <AddTask />
-      <TaskList />
+      <Button
+      onClick={(e)=>{toggle()}} variant="dark">{text}</Button>
+      { isOpen && <AddTask/>}
+      <TaskList task={task}/>
+          
     </div>
   );
 };
