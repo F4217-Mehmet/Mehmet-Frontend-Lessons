6378eb72dbb4e63e222ae06750eef80114aa708e
Mehmet Kabasakal <sehriistanbul@gmail.com>
2022-10-22 11:39:38 +0300

    route start

--- react/class-notes/rc10_task_tracker/src/App.js
+++ react/class-notes/rc10_task_tracker/src/App.js
@@ -2,6 +2,7 @@ import Header from './components/header/Header';
 import Home from './page/Home'
 
 
+
 function App() {
   return (
     <div className="container">
--- react/class-notes/rc10_task_tracker/src/page/Home.jsx
+++ react/class-notes/rc10_task_tracker/src/page/Home.jsx
@@ -1,39 +1,39 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import AddTask from "../components/addTask/AddTask";
 import TaskList from "../components/taskList/TaskList";
 import Button from "react-bootstrap/Button"
-import { useState } from "react";
-import { useEffect } from "react";
 import axios from "axios";
 
 const Home = () => {
-  const [isOpen, setIsOpen]= useState(false)
-  const [text, setText]=useState("Show Task Bar")
-  const [task, setTask]= useState();
-  const url = "https://6351820a3e9fa1244e60840c.mockapi.io/api/tasks";
+  const [isOpen,setIsOpen]=useState(false)
+  const [text,setText]=useState("Show Task Bar")
+  const [task,setTask]= useState([]);
+  const url="https://63516c99dfe45bbd55bfd9a2.mockapi.io/api/tasks"
 
-  const toggle=(e)=>{
-    setIsOpen(!isOpen)
+  const toggle=()=>{
+    setIsOpen(!isOpen);
     const buttonText=isOpen ? "Show Task Bar" : "Hide Task Bar"
     setText(buttonText);
   }
 
-  const getTask= async()=>{
+  // CRUD  -READ--
+  const getTask=async()=>{
     const {data}=await axios(url)
     setTask(data);
-    console.log(data);
+    console.log(data)
   }
 
   useEffect(() => {
     getTask()
   },[])
+
   return (
-    <div>
-      <Button
-      onClick={(e)=>{toggle()}} variant="dark">{text}</Button>
-      { isOpen && <AddTask/>}
-      <TaskList task={task}/>
-          
+    <div className="mt-4 d-flex justify-content-center flex-column">
+      <Button 
+      onClick={()=>{toggle()}}
+      variant="danger">{text}</Button>
+      { isOpen && <AddTask getTask={getTask}/>}     
+      <TaskList task={task} getTask={getTask}/>
     </div>
   );
 };
